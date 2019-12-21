267e50fec5e902fd19f29e17bbd63d8b60f936a7
Aldo Huayhua <aldohuayhua@gmail.com>
2019-12-21 14:04:25 -0500

    connected seller form to posts

--- public/assets/js/seller.js
+++ public/assets/js/seller.js
@@ -1,32 +1,26 @@
-$(document).ready(function () {
-    $("#addBuyer").on("submit", function (event) {
-        // Make sure to preventDefault on a submit event.
-        event.preventDefault();
-        // [name=plan] will find an element with a "name" attribute equal to the string "plan"
-        var newSeller = {
-            seller_name: $("#sellerName")
-                .val()
-                .trim(),
-            seller_phone: $("#sellerPhone")
-                .val()
-                .trim(),
-            product_name: $("#productName")
-                .val()
-                .trim(),
-            product_discription: $("#productDescription")
-                .val()
-                .trim(),
-        };
-        // Send the POST request.
-        $.ajax("/seller", {
-            type: "POST",
-            data: JSON.stringify(newSeller),
-            dataType: "json",
-            contentType: "application/json"
-        }).then(function () {
-            console.log("created new product");
-            // Reload the page to get the updated list
-            location.reload();
-        });
-    });
-});
+// $(document).ready(function () {
+//     $("#addBuyer").on("submit", function (event) {
+//         // Make sure to preventDefault on a submit event.
+//         event.preventDefault();
+//         // [name=plan] will find an element with a "name" attribute equal to the string "plan"
+//         var newSeller = {
+//             seller_name: $("addProduct [name=sellerName]")
+//                 .val()
+//                 .trim(),
+//             seller_phone: $("addProduct [name=sellerPhone]")
+//                 .val()
+//                 .trim(),
+//         };
+//         // Send the POST request.
+//         $.ajax("/seller_api", {
+//             type: "POST",
+//             data: JSON.stringify(newSeller),
+//             dataType: "json",
+//             contentType: "application/json"
+//         }).then(function () {
+//             console.log("created new seller");
+//             // Reload the page to get the updated list
+//             location.reload();
+//         });
+//     });
+// });
