753ef1c9560e38e1e12f5b75ed4fcc5318912772
Aldo Huayhua <aldohuayhua@gmail.com>
2019-12-23 20:26:59 -0500

    added timestamp

--- controllers/controller.js
+++ controllers/controller.js
@@ -5,8 +5,6 @@ var router = express.Router();
 
 var path = require("path")
 var products = require("../models/product");
-var sellers= require("../models/seller");
-var buyers= require("../models/buyer");
 
 // HOME PAGE
 router.get("/", function (req, res) {
@@ -28,24 +26,12 @@ router.get("/products_api", function (req, res) {
     res.json({ product: data });
   });
 });
-// SELLER API
-router.get("/seller_api", function (req, res) {
-    sellers.all(function (data) {
-      res.json({ seller: data });
-    });
-  });
- // BUYER API
-  router.get("/buyer_api", function (req, res) {
-    buyers.all(function (data) {
-      res.json({ buyer: data });
-    });
-  });
   //POST PRODUCTS
 router.post("/products_api", function(req, res) {
   products.create([
-    "product_name", "product_description", "product_image","highest_bid", "seller_id", "buyer_id"
+    "product_name", "product_description", "product_image","highest_bid", "seller_name", "seller_phone"
   ], [
-    req.body.product_name, req.body.product_description, req.body.product_image ,req.body.highest_bid, req.body.seller_id, req.body.buyer_id
+    req.body.product_name, req.body.product_description, req.body.product_image ,req.body.highest_bid, req.body.seller_name, req.body.seller_phone
   ], function(result) {
     // Send back the ID of the new quote
     // res.json({ id: result.insertId });
@@ -53,40 +39,21 @@ router.post("/products_api", function(req, res) {
   });
 });
  //POST buyers
- router.post("/buyer_api", function(req, res) {
-  console.log("Buyer Post")
-  buyers.create([
-    "buyer_name", "buyer_phone", "product_id"
-  ], [
-    req.body.buyer_name, req.body.buyer_phone, req.body.product_id
-  ], function(result) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
+ router.put("/products_api/:id", function(req, res) {
+  console.log("Buyer Update")
+  var condition = "product_id = " +req.params.id;
+  console.log("condition", condition);
+  products.update({
+    // [
+    highest_bid: req.body.highest_bid,
+    buyer_name: req.body.buyer_name,
+    buyer_phone: req.body.buyer_phone,
+    moment_bid: req.body.moment_bid
+}, condition, function(result) {
     console.log(result)
   });
 });
-// router.post("/Product_Update_api", function(req, res) {
-//   product.update([
-//     "buyer_name",  "buyer_phone", "product_id", 
-//   ], [
-//     req.body.product_name, req.body.product_description, req.body.highest_bid, req.body.seller_id, req.body.buyer_id
-//   ], function(result) {
-//     // Send back the ID of the new quote
-//     res.json({ id: result.insertId });
-//     console.log(result)
-//   });
-// });
-router.post("/seller_api", function(req, res) {
-  sellers.create([
-    "seller_name", "seller_phone", "product_id"
-  ], [
-    req.body.seller_name, req.body.seller_phone, req.body.product_id
-  ], function(result) {
-    // Send back the ID of the new quote
-    // res.json({ id: result.insertId });
-    console.log(result)
-  });
-});
+
 
 module.exports = router;
 
--- public/assets/js/product.js
+++ public/assets/js/product.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
     // using form in seller page to fill out the products table and seller table
     $("#addProduct").on("submit", function (event) {
         // Make sure to preventDefault on a submit event.
@@ -17,12 +18,15 @@ $(document).ready(function () {
             highest_bid: $("#addProduct [name=minBid]")
                 .val()
                 .trim(),
-            seller_id: $("#addProduct [name=sellerPhone]")
+            seller_name: $("#addProduct [name=sellerName]")
                 .val()
                 .trim(),
-            buyer_id: $("#addProduct [name=sellerPhone]")
+            seller_phone: $("#addProduct [name=sellerPhone]")
                 .val()
                 .trim()
+            // posted_since: $("#addProduct [name=sellerPhone]")
+            // .val()
+            // .trim()
         };
         // Send the POST request.
         $.ajax("/products_api", {
@@ -33,38 +37,44 @@ $(document).ready(function () {
         }).then(function () {
             location.reload();
         });
-        var newSeller = {
-            seller_name: $("#addProduct [name=sellerName]")
-                .val()
-                .trim(),
-            seller_phone: $("#addProduct [name=sellerPhone]")
-                .val()
-                .trim()
-        };
-        $.ajax("/seller_api", {
-            type: "POST",
-            data: JSON.stringify(newSeller),
-            dataType: "json",
-            contentType: "application/json"
-        }).then(function () {
-            console.log("created new seller");
-            // Reload the page to get the updated list
-            location.reload();
-        });
+        // var newSeller = {
+        //     seller_name: $("#addProduct [name=sellerName]")
+        //         .val()
+        //         .trim(),
+        //     seller_phone: $("#addProduct [name=sellerPhone]")
+        //         .val()
+        //         .trim()
+        // };
+        // $.ajax("/seller_api", {
+        //     type: "POST",
+        //     data: JSON.stringify(newSeller),
+        //     dataType: "json",
+        //     contentType: "application/json"
+        // }).then(function () {
+        //     console.log("created new seller");
+        //     // Reload the page to get the updated list
+        //     location.reload();
+        // });
     });
 
     $.ajax("/products_api", {
         type: "GET"
+
     }).then(function (data) {
         console.log(data)
         var product = data.product;
         var len = product.length;
         var products_elem = $("#products");
         for (var i = 0; i < len; i++) {
+            // Split timestamp into [ Y, M, D, h, m, s ]
+            var t = product[i].posted_since.split(/[- :]/);
+            // Apply each element to the Date function
+            // var d = new Date(Date.UTC(t[0], t[1] - 1, t[2], t[3], t[4], t[5]));
+            console.log(t);
             products_elem.append(
                 "<div class='col-sm-3'>" +
                 "<div  class='card' style='width: 18rem; height:100%'>" +
-                "<img src=" + 
+                "<img src=" +
                 product[i].product_image +
                 " class='card-img-top' alt='...'>" +
                 "<div class='card-body'>" +
@@ -76,14 +86,15 @@ $(document).ready(function () {
                 product[i].product_description + "</p>" +
                 "<h5 class='card-title'>" + "Current Bid $" +
                 product[i].highest_bid + "</h5>" +
-                // "<br>" +
-                // "<div class='card-footer'>" +
-                "<a type='button' class='btn btn-primary text-white' data-toggle='modal' data-target='#exampleModal'>" +
-                product[i].product_id + "BID" + "</a>" +
+                "<p>Posted at: " + product[i].posted_since + "</p>" +
+                "<p>Last Bid at: " + product[i].moment_bid + "</p>" +
+                "<a type='button' id='submitBuyer' class='btn btn-primary text-white' data-toggle='modal' data-target='#exampleModal'" +
+                "data-id=" + product[i].product_id + ">" + "BID" + "</a>" +
                 // "</div>" +
                 "</div>" +
                 "</div>"
             );
         }
     });
+    $("#addProduct").val("");
 });
