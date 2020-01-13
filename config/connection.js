038ee61f10ce3844643597353846d2b130ba2c39
Aldo Huayhua <aldohuayhua@gmail.com>
2020-01-12 19:12:52 -0500

    added styling

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,17 @@ var mysql = require("mysql");
 
 var connection;
 
-connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "ebay_db"
-});
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "ebay_db"
+  });  
+}
 
 // Make connection.
 connection.connect(function(err) {
